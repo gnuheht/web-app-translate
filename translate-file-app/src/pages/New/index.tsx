a57361eca4d8534cb68a45ee0726bd8422bc1fa3
gnuheht <bungphehunghi@gmail.com>
2023-10-16 18:02:35 +0700

    done set up New

--- translate-file-app/src/pages/New/index.tsx
+++ translate-file-app/src/pages/New/index.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import NewLeft from './new-left';
+import NewRight from './new-right';
+import NewMain from './new-main';
 
 const New = () => {
-  return <div className="con"></div>;
+  return (
+    <div className="container">
+      <div className="w-9/12 ">
+        <div className="w-full flex flex-row">
+          <div>
+            <NewLeft />
+          </div>
+          <div>
+            <NewMain />
+          </div>
+          <div>
+            <NewRight />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default New;
